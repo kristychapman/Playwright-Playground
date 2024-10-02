1ddccae64e9611a2f769cfb452c0ede9c098f759
kchapman <kchapman@KPERS.ORG>
2024-10-02 07:34:24 -0500

    verify about page

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -9,6 +9,14 @@ test.describe('Home', () => {
     //verify url
     await expect(page).toHaveTitle('Practice E-Commerce Site – SDET Unicorns');
     })
+
+test('Open About page and verify title', async ({ page}) => {
     
-    
+    //open url
+    await page.goto('https://practice.sdetunicorns.com/about/');
+
+    //verify page
+    await expect(page).toHaveTitle('About – Practice E-Commerce Site');
 })
+})
+
